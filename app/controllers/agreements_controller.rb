edfd004e10fe1ffc4e76c7bea330051908a152d1
Danny Cardella <dcardella@endeavorequity.com>
2012-05-31 11:25:01 -0500

    Andrew's Handywork... Added Form for Inviting Users to Agreements

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -3,13 +3,23 @@ class AgreementsController < ApplicationController
   end
 
   def new
-    @agreement = Agreement.new
+    
   end
 
   def edit
-    @agreement = Agreement.find_by_id(params[:id])
   end
 
   def show
+    @agreement = Agreement.find_by_id(params[:id])
+  end
+  
+  def create
+      @agreement = Agreement.new(params[:agreement])
+      if @agreement.save
+        redirect_to projects_url
+      else
+        flash.now alert: "Something has gone terribly wrong"
+        render "new"
+      end
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,21 +10,12 @@ class ProjectsController < ApplicationController
   end
   
   def create
-
     @project = Project.new(params[:project])
       
     if @project.save
       Agreement.create title: "Group Admin", description: "I'm the Admin of this Project", is_admin: true, project_id: @project.id, user_id: session[:uid]
       redirect_to projects_url
       return
-    else
-      flash.now alert: "Something has gone terribly wrong"
-      render "new"
-    end
-    
-    @agreement = Agreement.new(params[:agreement])
-    if @agreement.save
-      redirect_to projects_url
     else
       flash.now alert: "Something has gone terribly wrong"
       render "new"
@@ -36,7 +27,7 @@ class ProjectsController < ApplicationController
   end
 
   def show
-  @project = Project.find_by_id(params[:id])
-  @agreement = Agreement.new
+    @project = Project.find_by_id(params[:id])
+    @agreement = Agreement.new
   end
 end
