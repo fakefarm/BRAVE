0dc6586ecc5bb3968d7fd7c05d259d1877c45212
Andrew Deitrick <ajdeitrick@gmail.com>
2012-06-03 18:23:09 -0500

    Fixing seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,12 @@ if Rails.env.development?
   names.each { |name, email|  User.create name: name, email: email, role: "magician", password: "tree", password_confirmation: "tree" }
   client = Client.create name: "Client", email: "Client@client.com"
   
-  amount = rand(1000..5000)
+  rand(1000..5000)
   
-  Project.create title: "Rails Project", client_email: client.email, client_id: client.id, is_active: true, project_amount: amount
-  Project.create title: "WordPress Project", client_email: client.email, client_id: client.id, is_active: true, project_amount: amount
-  Project.create title: "Design Project", client_email: client.email, client_id: client.id, is_active: true, project_amount: amount
-  Project.create title: "Beer and Donuts Project", client_email: client.email, client_id: client.id, is_active: true, project_amount: amount
+  Project.create title: "Rails Project", client_email: client.email, client_id: client.id, is_active: true, project_amount: rand(1000..5000)
+  Project.create title: "WordPress Project", client_email: client.email, client_id: client.id, is_active: true, project_amount: rand(1000..5000)
+  Project.create title: "Design Project", client_email: client.email, client_id: client.id, is_active: true, project_amount: rand(1000..5000)
+  Project.create title: "Beer and Donuts Project", client_email: client.email, client_id: client.id, is_active: true, project_amount: rand(1000..5000)
   
   projects = Project.all
   project_manager = User.find_by_name("The Boss")
@@ -23,7 +23,7 @@ if Rails.env.development?
   projects.each do |project|
     Agreement.create title: "Project Manager", description: "I'm in charge of everything", 
                      content: "I'll do such and such, make sure such and such is great, and I'll also provide the beer and donuts",
-                     user_id: project_manager.id, project_id: project.id, is_admin: true, user_amount: 1000  
+                     user_id: project_manager.id, project_id: project.id, is_admin: true, user_amount: 1000
     end
-puts "WOOP"
+puts "BE BRAVE GENTLEMEN. THIS GUILD ISN'T GOING TO BUILD ITSELF...oh, and also, data has been added"
 end
