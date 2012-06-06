6942a07bc6533ef4c6d833009fed93594968151f
Andrew Deitrick <ajdeitrick@gmail.com>
2012-06-06 16:20:35 -0500

    fixing static_pages index view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,12 @@
 class StaticPagesController < ApplicationController
 
-    before_filter :require_sign_in, :except => [:index, :about]
+  before_filter :require_sign_in, :except => [:index, :about]
 
   def index
 
   end
-  
+
   def about
-    
+
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_filter :require_sign_in, :except => [:new, :create]
+  
   def index
     @users = User.all
   end
