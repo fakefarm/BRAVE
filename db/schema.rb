1396c442ca67d7d573fb02049549d2d5e349305a
Danny Cardella <dcardella@endeavorequity.com>
2012-06-12 14:52:39 -0500

    Nits on Views

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,8 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
+ActiveRecord::Schema.define(:version => 20120611195528) do
+
   create_table "agreements", :force => true do |t|
     t.string   "title"
     t.string   "description"
