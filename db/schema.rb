86d3231e4fa6095a6eda6046ca96a225a1d89851
Dave Woodall <dmwoodall@gmail.com>
2012-06-13 10:17:02 -0500

    use session hash for conditional

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120604193808) do
+ActiveRecord::Schema.define(:version => 20120530205328) do
 
   create_table "agreements", :force => true do |t|
     t.string   "title"
@@ -30,13 +30,6 @@ ActiveRecord::Schema.define(:version => 20120604193808) do
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-  end
-
-  create_table "members", :force => true do |t|
-    t.integer  "agreement_id"
-    t.integer  "user_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
   end
 
   create_table "projects", :force => true do |t|
